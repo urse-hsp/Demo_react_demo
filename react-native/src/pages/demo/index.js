4ce3af5f41c82dd1b20837f5e1351918ce88f37e
侯少平 <hsp_email@163.com>
2022-12-17 20:28:41 +0800

    init

---
+++ react-native/src/pages/demo/index.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const Demo: () => Node = () => {
+  return <Text>Demo</Text>;
+};
+export default Demo;
